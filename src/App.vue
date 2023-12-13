f4296ffc88be86333714882de398265f99dee49d
kan327 <kannybaihaqi100@gmail.com>
2023-12-13 22:22:48 +0700

    nav responsive mobile

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,12 @@
   onUnmounted(() => {
     window.removeEventListener('scroll', handleScroll);
   });
+
+  let menuDropDown = ref(true);
+
+  const toggleMenu = () => {
+    menuDropDown.value = !menuDropDown.value;
+  };
 </script>
 
 <template>
@@ -32,10 +38,29 @@
           Contact
         </RouterLink>
       </div>
-      <p class="text-2xl font-black text-b-37">Landing</p>
+      <div class="flex items-center gap-3">
+        <span class="material-symbols-outlined text-4xl cursor-pointer select-none" @click="toggleMenu" v-if="menuDropDown">
+          menu
+        </span>
+        <span class="material-symbols-outlined text-4xl cursor-pointer select-none" @click="toggleMenu" v-if="!menuDropDown">
+          close
+        </span>
+        <p class="text-2xl font-black text-b-37">Landing</p>
+      </div>
       <button class="bg-b-11 min-w-[180px] px-9 py-1 text-white font-semibold rounded-sm">Buy Now</button>
       <!-- <button class="bg-b-11 px-16 py-2 text-white font-semibold rounded-sm">Buy Now</button> -->
     </div>
+    <div v-if="menuDropDown" class="nav_item">
+        <RouterLink to="/" class="bg_blur w-full block py-3 px-6 text-sm text-b-50 border-b-2 border-solid border-b-09">
+          Home
+        </RouterLink>
+        <RouterLink to="/about" class="bg_blur w-full block py-3 px-6 text-sm text-b-50 border-b-2 border-solid border-b-09">
+          About
+        </RouterLink>
+        <RouterLink to="/contact" class="bg_blur w-full block py-3 px-6 text-sm text-b-50 border-b-2 border-solid border-b-09">
+          Contact
+        </RouterLink>
+      </div>
   </nav>
   <RouterView />
 </template>
@@ -43,5 +68,14 @@
 <style scoped>
 .bg_blur {
   @apply backdrop-blur-md
+}
+.nav_item .bg_blur:nth-child(1) {
+  @apply backdrop-blur-xl
+}
+.bg_blur {
+  @apply backdrop-blur-2xl
+}
+.bg_blur {
+  @apply backdrop-blur-3xl
 }
 </style>
