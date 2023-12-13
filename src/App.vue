c267c487109b7715f3fabe0d65f020961a6bcdc5
kan327 <kannybaihaqi100@gmail.com>
2023-12-13 11:56:03 +0700

    section card

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   import { ref, onMounted } from 'vue'
   import { RouterLink, RouterView } from 'vue-router'
 
+  import SectionCard from './components/Section-card.vue'
 </script>
 
 <template>
   <!-- <nav class="flex justify-between  items-center py-5 px-32 backdrop-blur-md fixed w-full top-0 z-50"> -->
-  <nav class="flex justify-between  items-center py-5 px-32 fixed w-full top-0 z-50">
+  <nav class="flex justify-between items-center py-5 px-32 fixed w-full top-0 z-50">
     <div class="gap-14 flex">
       <RouterLink to="/" class="text-sm text-b-50">
         Home
@@ -22,15 +23,19 @@
     <button class="bg-b-11 min-w-[180px] px-9 py-1 text-white font-semibold rounded-sm">Buy Now</button>
     <!-- <button class="bg-b-11 px-16 py-2 text-white font-semibold rounded-sm">Buy Now</button> -->
   </nav>
-  <header class="h-screen ml-32 items-center flex relative overflow-x-hidden">
-    <div class="w-[45%] max-w-3xl h-fit">
+  <!-- after:contents-[''] after:block after:pb-[100%] -->
+  <header class="h-[110vh] overflow-clip ml-32 items-center flex relative overflow-x-hidden">
+    <div class="w-[45%] max-w-3xl h-fit relative">
       <h1 class="font-semibold text-5xl text-b-09">Introduce Your Product Quickly & Effectively</h1>
-      <p class="mt-5 my-10 text-lg">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus <br> mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+      <p class="mt-5 mb-10 text-lg">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus <br><br> mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
       <div class="gap-7 flex">
         <button class="bg-b-11 min-w-[180px] py-1 text-white font-semibold rounded-sm">Purchase UI Kit</button>
         <button class="border-b-09 border-2 min-w-[180px] py-1 text-b-09 font-semibold rounded-sm">Learn More</button>
       </div>
     </div>
-    <img class="absolute -z-10 -right-[25%] -top-1/3 w-[100%]" src="/images/hero(fixed).svg" alt="">
+    <img class="absolute -z-10 -right-[25%] -top-1/3 w-[100%]" :src="`/images/hero(fixed).svg`" alt="">
   </header>
+  <main class="overflow-clip">
+    <SectionCard></SectionCard>
+  </main>
 </template>
---
+++ src/components/Section-card.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <section class="px-32 py-52 relative">
+    <div class="w-1/2">
+      <h2 class="font-semibold text-4xl text-b-09">Light, Fast & Powerful</h2>
+      <p class="mt-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus <br><br> mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+    </div>
+    <img class="absolute top-40 -z-10 right-32" :src="`/images/list-1.svg`" alt="">
+    <div class="w-1/2 py-20 flex justify-between">
+      <div class="w-[48%]">
+        <img :src="`/images/title-icon.svg`" alt="">
+        <h3 class="font-semibold text-b-09 mt-4 mb-2">Title Goes Here</h3>
+        <p class="text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+      </div>
+
+      <div class="w-[48%]">
+        <img :src="`/images/title-icon.svg`" alt="">
+        <h3 class="font-semibold text-b-09 mt-4 mb-2">Title Goes Here</h3>
+        <p class="text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+      </div>
+    </div>
+  </section>
+</template>
